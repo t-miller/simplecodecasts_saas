9bf1a567da7597605f61f293e23f22765d9ad779
t-miller <tyler.rw.miller@gmail.com>
2016-08-09 00:38:47 +0000

    fixing This customer has no attached payment source error

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -12,7 +12,7 @@ $(document).ready(function() {
 
     if (!error) {
       // Get the Stripe token:
-      Stripe.createToken({
+      Stripe.card.createToken({
         number: ccNum,
         cvc: cvcNum,
         exp_month: expMonth,
@@ -24,14 +24,14 @@ $(document).ready(function() {
   
   function stripeResponseHandler(status, response) {
     // Get a reference to the form:
-    var f = $("#new_user");
+    var f = $(".new_user");
 
     // Get the token from the response:
     var token = response.id;
 
     // Add the token to the form:
     f.append('<input type="hidden" name="user[stripe_card_token]" value="' + token + '" />');
-
+    
     // Submit the form:
     f.get(0).submit(); 
   }
